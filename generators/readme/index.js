5b95bd88b25b671418c412f4bdf054f031f14d49
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-05 16:01:06 +0200

    feat(source): release 0.10.9

--- generators/readme/index.js
+++ generators/readme/index.js
@@ -3,28 +3,22 @@ const Generator = require('yeoman-generator');
 const messages = require('./templates/messages');
 
 module.exports = class Readme extends Generator {
-    constructor(args, options) {
-        super(args, options);
-
-        this.option('project', {
-            description:
-                'Name of the project README.md file is generating for.',
-            type:    String,
-            default: this.config.get('repositoryName') || 'clean-install',
-        });
-    }
-
     writing() {
-        const { project } = this.options;
-
         const initialized = this.config.get('initialized');
         const educational = this.config.get('educational');
         const redux = this.config.get('redux');
         const webpack = this.config.get('webpack');
         const repositoryOwner = this.config.get('repositoryOwner');
-        const repositoryName = this.config.get('repositoryName');
 
-        const selectedMessages = messages[ project ];
+        let repositoryName = 'clean-install';
+        const isPackageJsonExists = this.fs.exists('package.json');
+
+        if (isPackageJsonExists && educational) {
+            const { name } = JSON.parse(this.fs.read('package.json'));
+            repositoryName = name;
+        }
+
+        const selectedMessages = messages[ repositoryName ];
 
         if (!initialized) {
             this.fs.copy(
@@ -42,7 +36,7 @@ module.exports = class Readme extends Generator {
         }
 
         this.fs.copyTpl(
-            this.templatePath(`${project}.ejs`),
+            this.templatePath(`${repositoryName}.ejs`),
             this.destinationPath('README.md'),
             {
                 ...selectedMessages,
