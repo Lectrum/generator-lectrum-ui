5d0f53ae206ef407ca2d167270f955317f739b7f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2018-11-14 17:06:44 +0200

    chore(source): update webpack config

--- generators/app/templates/source/index.js
+++ generators/app/templates/source/index.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Theme
-import './theme/init';
-
-const start = <div className = 'start'>Добро пожаловать!</div>;
+const start = <div>Добро пожаловать!</div>;
 
 ReactDOM.render(start, document.getElementById('app'));
